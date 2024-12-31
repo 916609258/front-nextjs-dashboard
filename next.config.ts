b4aa347e1edeadce4f2edff7009cd503e511f7cb
xhh <916609258@qq.com>
2024-12-31 14:05:47 +0800

    ESLint Error 问题

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // 经过尝试在该文件增加下面的代码，不能解决 ESLint 的Error 问题，
+  // 需要修改 eslint.config.mjs 文件中的配置。
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
 
 };
 
